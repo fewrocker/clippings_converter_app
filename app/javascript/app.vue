6f18a7b65e3b3aef63ef7c18664d1409781ae423
Felipe Santos <santos.felipe13@gmail.com>
2019-06-13 20:42:02 -0300

    Refactored main vue app to separate in different pages

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,11 @@
 <template>
   <div>
     <div class="full-app">
-      <div v-if="screen === 'input-file'" class="app-input-file text-center">
-        <h1 class="welcome-title">
-          Please upload you "my_clippings.txt" file below:
-        </h1>
-        <input type="file" ref="fileInput" @change="processFile($event)">
-      </div>
+      <UploadFile v-if="screen === 'input-file'" @books-loaded="listBooks"/>
 
-      <div v-if="screen === 'list-books'" class="app-list-books text-center">
+      <ListBooks v-if="screen === 'list-books'" :books="books"/>
+
+<!--       <div v-if="screen === 'list-books'" class="app-list-books text-center">
         <h1 class="welcome-title">
           Here are the books contained in your clippings:
         </h1>
@@ -17,47 +14,32 @@
           <h1 class="book-name"> {{book.name}} </h1>
           <h2 class="book-highlights"> {{book.highlight_count}} </h2>
         </div>
-      </div>
+      </div> -->
     </div>
-
   </div>
 </template>
 
 <script>
+import UploadFile from 'pages/upload_file.vue'
+import ListBooks from 'pages/list_books.vue'
+
 export default {
   data: function () {
     return {
       screen: "input-file",
-      message: "Hello Vue!",
-      file: "",
-      content: "",
       books: [],
     }
   },
+  components: {
+    UploadFile,
+    ListBooks,
+  },
   methods: {
-    async processFile(event) {
-      const vm = this
-
-      const file = event.target.files[0];
-      const reader = new FileReader();
-
-      reader.onload = e => (this.content = e.target.result);
-      reader.readAsText(file);
-
-      await this.$sleep(1000)
-
-      const res = await axios({
-        method: 'post',
-        url: '/return_books',
-        data: {
-          content: this.content,
-        }
-      })
-
-      this.books = res.data.books
+    listBooks(books) {
+      console.log(books)
       this.screen = "list-books"
+      this.books = books
     },
-
     async downloadBook(bookId) {
       const res = await axios({
         method: 'post',
